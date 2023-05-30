2782ccedc29a8d07ff8f9b80c0e51b111dc22804
unknown <fernando.seguinsf@gmail.com>
2023-05-30 09:37:57 -0300

    first commit

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,21 @@
+
+class Pokemon {
+    number;
+    name;
+    types;
+    types = [];
+    image;
+}
+
+class Details {
+    number;
+    name;
+    image; 
+    type;
+    types = [];
+    height;
+    weight;
+    hp;
+    attack;
+    defense;       
+  }
